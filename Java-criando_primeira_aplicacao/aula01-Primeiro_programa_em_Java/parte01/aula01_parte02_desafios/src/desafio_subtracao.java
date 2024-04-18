1ad1416e5202b092cd5cb56a1a967d81e1cc487c
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-18 01:14:31 -0300

    Fix: Rename classes to follow Java naming conventions

--- Java-criando_primeira_aplicacao/aula01-Primeiro_programa_em_Java/parte01/aula01_parte02_desafios/src/desafio_subtracao.java
+++
@@ -1,7 +0,0 @@
-public class desafio_subtracao
-{
-    public static void main(String[] args)
-    {
-        System.out.println(10 - 5);
-    }
-}
--- Java-criando_primeira_aplicacao/aula01-Primeiro_programa_em_Java/parte02_desafios/src/Subtracao.java
+++ Java-criando_primeira_aplicacao/aula01-Primeiro_programa_em_Java/parte02_desafios/src/Subtracao.java
@@ -1,7 +1,7 @@
-public class desafio_soma
+public class Subtracao
 {
     public static void main(String[] args)
     {
-        System.out.println(10 + 5);
+        System.out.println(10 - 5);
     }
 }
