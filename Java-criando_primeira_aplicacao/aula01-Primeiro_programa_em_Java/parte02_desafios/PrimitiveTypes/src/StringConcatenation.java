ed5d6ec0a37a80636f1b601e890b43eb7cee56a8
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-25 07:41:35 -0300

    Refactor StringConcatenation class to use Scanner for user input and improve variable naming

--- Java-criando_primeira_aplicacao/aula01-Primeiro_programa_em_Java/parte02_desafios/PrimitiveTypes/src/StringConcatenation.java
+++ Java-criando_primeira_aplicacao/aula01-Primeiro_programa_em_Java/parte02_desafios/PrimitiveTypes/src/StringConcatenation.java
@@ -1,17 +1,30 @@
 /*
-**  Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra).
-**  Atribua valores a essas variáveis e concatene-as em uma mensagem.
+**  BRAZILIAN PORTUGUESE:
+**      Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra).
+**      Atribua valores a essas variáveis e concatene-as em uma mensagem.
+**
+**  ENGLISH:
+**      Declare a variable of type char (letter) and a String variable (word).
+**      Assign values to these variables and concatenate them in a message.
 */
+
+import java.util.Scanner;
 
 public class StringConcatenation
 {
     public static void main(String[] args)
     {
-        char    article;
-        String  phrase;
+        char    letter;
+        String  word;
+        Scanner keyboard;
 
-        article = 'A';
-        phrase = "man is walking down the street.";
-        System.out.printf("%s %s\n", article, phrase);
+        keyboard = new Scanner(System.in);
+        System.out.print("Letter: ");
+        letter = keyboard.next().charAt(0);
+        keyboard = new Scanner(System.in);
+        System.out.print("Word: ");
+        word = keyboard.nextLine();
+        keyboard.close();
+        System.out.printf("Letter + Word => %c %s\n", letter, word);
     }
 }
