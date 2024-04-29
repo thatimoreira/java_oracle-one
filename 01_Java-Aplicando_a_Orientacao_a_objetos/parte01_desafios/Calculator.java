cb4f2700539232d2dabc3b033d46693046e9357f
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-29 17:44:24 -0300

    Add task 2 solution.

---
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/parte01_desafios/Calculator.java
@@ -0,0 +1,5 @@
+public class Calculator {
+    double  doubleNumber(double num){
+        return (2 * num);
+    }
+}
--- 01_Java-Aplicando_a_Orientacao_a_objetos/parte01_desafios/Main.java
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/parte01_desafios/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Person  person1 = new Person();
         person1.greeting();
+
+        Calculator calculator1 = new Calculator();
+        double n1 = 5.3;
+        System.out.printf("2 * %f = %f\n", n1, calculator1.doubleNumber(n1));
     }
 }
