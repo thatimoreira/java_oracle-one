dd083f5b0b4592954f3821ee3a16193917256ec8
thatimoreira <thatimoreira.dev@gmail.com>
2024-04-29 22:23:33 -0300

    Enhance Main class to clearly separate label each class demonstration, improving readability and organization.

--- 01_Java-Aplicando_a_Orientacao_a_objetos/parte01_desafios/Main.java
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/parte01_desafios/Main.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.print("\n---------------- PERSON CLASS ----------------\n");
         Person  person1 = new Person();
         person1.greeting();
 
-        Calculator calculator1 = new Calculator();
-        double n1 = 5.3;
+
+        System.out.print("\n-------------- CALCULATOR CLASS --------------\n");
+        Calculator  calculator1 = new Calculator();
+        Scanner     scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        double n1 = scanner.nextDouble();
         System.out.printf("2 * %f = %f\n", n1, calculator1.doubleNumber(n1));
 
+        System.out.print("\n----------------- MUSIC CLASS -----------------\n");
         Music   music1 = new Music();
         double  newRate;
         music1.musicTitle = "My Hood (feat. Stormzy)";
@@ -15,5 +23,15 @@ public class Main {
         newRate = music1.rateMusic();
         music1.rating = music1.calculateRatingAverage(newRate);
         music1.displayTechnicalSheet();
+
+        System.out.print("\n---------------------- CAR CLASS ---------------\n");
+        Car car1 = new Car();
+        car1.model = "Tiggo 7 Pro Hybrid Max Drive";
+        car1.modelYear = 2023;
+        car1.color = "Blue";
+        car1.calculateCarAge();
+        car1.displayTechnicalSheet();
+
+        scanner.close();
     }
 }
