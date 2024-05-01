c63dd6c2151b9c0801babd10ae45b925dc52e58a
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-01 16:22:42 -0300

    Exercise 1 solution. Created public and private field and generated getters and setters for the private ones

---
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/parte02_desafios/src/br/com/alura/javafundamentals/part02/BankAccount.java
@@ -0,0 +1,23 @@
+package br.com.alura.javafundamentals.parte02;
+
+public class BankAccount {
+    private int     accountNumber;
+    private double  balance;
+    public String   accountHolder;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
