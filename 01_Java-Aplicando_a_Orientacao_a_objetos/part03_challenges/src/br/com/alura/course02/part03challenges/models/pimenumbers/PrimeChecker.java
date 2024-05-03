f6e3b2bc141a94e4cb41b2bde5d1b3e4e1bf86e7
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-03 14:14:19 -0300

    Implement PrimeChecker class with a method to check if a number is prime, showcasing polymorphism to provide specific implementations.

--- 01_Java-Aplicando_a_Orientacao_a_objetos/part03_challenges/src/br/com/alura/course02/part03challenges/models/pimenumbers/PrimeChecker.java
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/part03_challenges/src/br/com/alura/course02/part03challenges/models/pimenumbers/PrimeChecker.java
@@ -1,2 +1,12 @@
-package br.com.alura.course02.part03challenges.models.pimenumbers;public class PrimeChecker {
+package br.com.alura.course02.part03challenges.models.pimenumbers;
+
+public class PrimeChecker extends PrimeNumbers{
+
+    public void  checkIfPrime(int num) {
+        if (checkPrimality(num)){
+            System.out.printf("%d is a prime number\n", num);
+        } else {
+            System.out.printf("%d is not a prime number\n", num);
+        }
+    }
 }
