59cf6d25b3b3e95c70c44ad0aadade9b66f51157
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-03 14:15:00 -0300

    Implement PrimeGenerator class with a method to generate the next prime number after a given number, highlighting inheritance to reuse properties and methods from PrimeNumbers.

--- 01_Java-Aplicando_a_Orientacao_a_objetos/part03_challenges/src/br/com/alura/course02/part03challenges/models/pimenumbers/PrimeGenerator.java
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/part03_challenges/src/br/com/alura/course02/part03challenges/models/pimenumbers/PrimeGenerator.java
@@ -1,2 +1,12 @@
-package br.com.alura.course02.part03challenges.models.pimenumbers;public class PrimeGenerator {
+package br.com.alura.course02.part03challenges.models.pimenumbers;
+
+public class PrimeGenerator extends PrimeNumbers {
+    public int generateNextPrime(int lastKnownPrimeNum){
+        int nextNum = ++lastKnownPrimeNum;
+
+        while (!checkPrimality(nextNum)){
+            nextNum++;
+        }
+        return nextNum;
+    }
 }
