18520577398426c2775efb49edae38b97d8a1acd
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-06 15:50:36 -0300

    Refactor Main class to use RectangularRoomCalculator for geometric calculations. Update method calls to use new class and interface, showcasing polymorphism and abstraction. This change promotes maintainability and flexibility in the codebase, adhering to the Open/Closed Principle (OCP) by allowing easy extension with new geometric calculations in the future.

--- 01_Java-Aplicando_a_Orientacao_a_objetos/part04_challenges/src/Main.java
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/part04_challenges/src/Main.java
@@ -1,8 +1,9 @@
-import br.com.alura.currencyconverter.CurrencyConverter;
+import br.com.alura.part04.currencyconverter.CurrencyConverter;
+import br.com.alura.part04.rectangularroomcalculator.RectangularRoomCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("------- CURRENCY CONVERTER -------");
+        System.out.println("\n------- CURRENCY CONVERTER -------");
         CurrencyConverter converter = new CurrencyConverter();
         converter.setDollarExchangeRate(5.07);
         converter.setDollarValue(100);
@@ -10,5 +11,19 @@ public class Main {
         System.out.printf("Value in dollars....: U$%.2f\n", converter.getDollarValue());
         System.out.printf("Value in Reais......: R$%.2f\n", converter.convertDollarToReal(converter.getDollarValue()));
 
+        System.out.println("\n--- RECTANGULAR ROOM CALCULATOR ---");
+        double area;
+        double perimeter;
+        RectangularRoomCalculator calculator = new RectangularRoomCalculator();
+        calculator.setHeightInMeters(2);
+        calculator.setWidthInMeters(4.5);
+        System.out.printf("Height = %.2fm\n", calculator.getHeightInMeters());
+        System.out.printf("Width = %.2fm\n", calculator.getWidthInMeters());
+        area = calculator.calculateArea(calculator.getHeightInMeters(), calculator.getWidthInMeters());
+        perimeter = calculator.calculatePerimeter(calculator.getHeightInMeters(), calculator.getWidthInMeters());
+        System.out.printf("Area = %.2fm²\n", area);
+        System.out.printf("Perimeter = %.2fm\n", perimeter);
+
+        System.out.println("\n------  MULTIPLICATION TABLE ------");
     }
 }
