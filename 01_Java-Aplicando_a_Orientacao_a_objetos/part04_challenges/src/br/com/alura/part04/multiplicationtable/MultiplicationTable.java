4bab60220d766236e5805d63bc804c499854001a
thatimoreira <thatimoreira.dev@gmail.com>
2024-05-15 17:00:02 -0300

    Missing 'Override' annotation. Insert this annotation.

--- 01_Java-Aplicando_a_Orientacao_a_objetos/part04_challenges/src/br/com/alura/part04/multiplicationtable/MultiplicationTable.java
+++ 01_Java-Aplicando_a_Orientacao_a_objetos/part04_challenges/src/br/com/alura/part04/multiplicationtable/MultiplicationTable.java
@@ -1,6 +1,7 @@
 package br.com.alura.part04.multiplicationtable;
 
 public class MultiplicationTable implements Table {
+    @Override
     public void    showMultiplicationTable(int num){
         for (int i = 0; i <= 10; i++){
             System.out.printf("%d x %d = %d\n", num, i, num * i);
