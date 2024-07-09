e229c2f741d90eaa59c0f4cab097336b1ee6885f
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-09 16:55:27 -0300

    Add exercise 1 solution - ArrayList with foreach loop.

---
+++ 02_Java-Trabalhando_com_listas_e_colecoes_de_dados/part03_challenges/Main.java
@@ -0,0 +1,18 @@
+package part03_challenges;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> stringsList = new ArrayList<>();
+        stringsList.add("Java");
+        stringsList.add("C");
+        stringsList.add("C++");
+        stringsList.add("C#");
+        stringsList.add("Python");
+
+        for (String item : stringsList) {
+            System.out.println(item);
+        }
+    }
+}
