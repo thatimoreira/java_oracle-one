36cf3db31b5de4d5b085b2425fec0652e5078092
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-09 17:02:20 -0300

    Implement exercise 2 half solution. Create Animal and Cachorro classes - Inheritance

---
+++ 02_Java-Trabalhando_com_listas_e_colecoes_de_dados/part03_challenges/Animal.java
@@ -0,0 +1,13 @@
+package part03_challenges;
+
+public class Animal {
+    private String nome;
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ 02_Java-Trabalhando_com_listas_e_colecoes_de_dados/part03_challenges/Cachorro.java
@@ -0,0 +1,14 @@
+package part03_challenges;
+
+public class Cachorro extends Animal{
+    private String raca;
+
+    public Cachorro(String raca) {
+        super(nome);
+        this.raca = raca;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+}
