6a987e8804d227ae3c17bb92ff647259c699da95
thatimoreira <thatimoreira.dev@gmail.com>
2024-07-10 02:46:07 -0300

    Create a list using the list interface, instantiate it as an ArrayList and as a Linked List.

--- 02_Java-Trabalhando_com_listas_e_colecoes_de_dados/part04_challenges/Main.java
+++ 02_Java-Trabalhando_com_listas_e_colecoes_de_dados/part04_challenges/Main.java
@@ -2,6 +2,8 @@ package part04_challenges;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -30,5 +32,19 @@ public class Main {
         for (Title title : titlesList) {
             System.out.println(title.getName());
         }
+
+        System.out.println("\n-------     Exercise 4     -------");
+        List<String> listArrayList = new ArrayList<>();
+        listArrayList.add("1st item");
+        listArrayList.add("2nd item");
+        listArrayList.add("3rd item");
+        
+        List<String> listLinkedList = new LinkedList<>();
+        listLinkedList.add("Element A");
+        listLinkedList.add("Element B");
+        listLinkedList.add("Element C");
+        
+        System.out.println("ArrayList: " + listArrayList);
+        System.out.println("LinkedList: " + listLinkedList);
     }
 }
